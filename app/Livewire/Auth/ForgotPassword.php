5daf8b2d7238cead611878ac9294afe261c2bbcf
hendriam <hendriam18@gmail.com>
2025-05-31 08:47:43 +0700

    feat: add page title in auth

--- app/Livewire/Auth/ForgotPassword.php
+++ app/Livewire/Auth/ForgotPassword.php
@@ -4,10 +4,12 @@ namespace App\Livewire\Auth;
 
 use Livewire\Component;
 use Livewire\Attributes\Layout;
+use Livewire\Attributes\Title;
 
 #[Layout('components.layouts.auth')]
 class ForgotPassword extends Component
 {
+    #[Title('Change Password')] 
     public function render()
     {
         return view('livewire.auth.forgot-password');
--- app/Livewire/Auth/Login.php
+++ app/Livewire/Auth/Login.php
@@ -4,10 +4,12 @@ namespace App\Livewire\Auth;
 
 use Livewire\Component;
 use Livewire\Attributes\Layout;
+use Livewire\Attributes\Title;
 
 #[Layout('components.layouts.auth')]
 class Login extends Component
 {
+     #[Title('Sign In')] 
     public function render()
     {
         return view('livewire.auth.login');
