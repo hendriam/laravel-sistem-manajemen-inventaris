ef98b78f4e29e622e3271bddf203a6f53beae695
hendriam <hendriam18@gmail.com>
2025-06-01 20:30:50 +0700

    feat: add "Remember me" checkbox to login form and enhance button styling

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -12,11 +12,19 @@
             <x-ui.input type="password" name="password" id="password" wire:model="password" class="p-2.5 w-full" placeholder="••••••••" required="" />
         </div>
         <div class="flex items-center justify-between">
+            <div class="flex items-start">
+                <div class="flex items-center h-5">
+                    <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800">
+                </div>
+                <div class="ml-3 text-sm">
+                    <label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
+                </div>
+            </div>
             @if (Route::has('forgot-password'))
                 <a href="{{ route('forgot-password') }}" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
             @endif
         </div>
-        <x-ui.button class="w-full">Sign in</x-ui.button>
+        <x-ui.button class="px-4 py-2.5 w-full bg-primary-700 hover:bg-primary-800 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</x-ui.button>
         <p class="text-sm font-light text-gray-500 dark:text-gray-400">
             Don’t have an account yet? <a href="#" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
         </p>
