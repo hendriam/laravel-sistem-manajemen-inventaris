f824a73a131df7be1b3cca5f1cba2342b3d4fcb6
hendriam <hendriam18@gmail.com>
2025-06-02 11:35:36 +0700

    feat: update table component to use dynamic columns and sorting functionality

--- resources/views/components/ui/table.blade.php
+++ resources/views/components/ui/table.blade.php
@@ -1,35 +1,27 @@
-@props([
-    'headers' => [],
-    'rows' => [],
-    'actions' => false,
-])
+@props(['data', 'columns', 'sortField' => null, 'sortDirection' => null])
 
 <div class="relative overflow-x-auto">
     <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
-                @foreach ($headers as $header)
-                    <th scope="col" class="px-4 py-3">{{ $header }}</th>
+                @foreach ($columns as $column)
+                    <th scope="col" wire:click="sortBy('{{ $column['field'] }}')"
+                        class="px-4 py-3 cursor-pointer hover:text-blue-500">
+                        {{ $column['label'] }}
+                        @if($column['field'] === $sortField)
+                            @if($sortDirection === 'asc')
+                                ↑
+                            @else
+                                ↓
+                            @endif
+                        @endif
+                    </th>
                 @endforeach
-                @if ($actions)
-                    <th class="px-4 py-3">Aksi</th>
-                @endif
+                <th class="px-4 py-3 text-gray-700">Aksi</th>
             </tr>
         </thead>
         <tbody>
-            @foreach ($rows as $row)
-                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
-                    @foreach ($row['data'] as $cell)
-                        <td scope="row" class="px-4 py-1 font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ $cell }}</td>
-                    @endforeach
-
-                    @if ($actions)
-                        <td class="px-4 py-1 max-w-[70px] ">
-                            {!! $row['actions'] ?? '' !!}
-                        </td>
-                    @endif
-                </tr>
-            @endforeach
+           {{ $slot }}
         </tbody>
     </table>
 </div>
