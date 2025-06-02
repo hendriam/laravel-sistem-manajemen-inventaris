1da1323792dbab72552c6f08301be4d6e073ac74
hendriam <hendriam18@gmail.com>
2025-06-02 16:36:20 +0700

    fix: remove primary key constraint from permission_role migration and adjust timestamps

--- database/migrations/2025_05_31_084822_create_permission_role_table.php
+++ database/migrations/2025_05_31_084822_create_permission_role_table.php
@@ -12,9 +12,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('permission_role', function (Blueprint $table) {
+            $table->id();
             $table->foreignId('role_id')->constrained()->cascadeOnDelete();
             $table->foreignId('permission_id')->constrained()->cascadeOnDelete();
-            $table->primary(['role_id', 'permission_id']);
+            $table->timestamps();
         });
     }
 
