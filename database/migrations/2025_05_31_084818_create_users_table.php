fb17ddec7eb022ac554066fe821b3b61f5227956
hendriam <hendriam18@gmail.com>
2025-06-03 13:32:19 +0700

    fix: update role_id foreign key constraint to restrict on delete

--- database/migrations/2025_05_31_084818_create_users_table.php
+++ database/migrations/2025_05_31_084818_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('username')->unique();
             $table->string('password');
-            $table->foreignId('role_id')->nullable()->constrained()->nullOnDelete();
+            $table->foreignId('role_id')->constrained()->onDelete('restrict');
             $table->timestamps();
         });
 
